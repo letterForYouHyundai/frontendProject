6ba5341f1874d57548c8de7c0f11c606b1e15730
y00n-lee <0613myday@gmail.com>
2023-12-11 23:00:29 +0900

    편지 작성 관련 페이지 수정

--- front-end-app/src/pages/letter/LetterCompleteRegistPage.jsx
+++ front-end-app/src/pages/letter/LetterCompleteRegistPage.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 
-const LetterCompleteRegistPage = () => <h1>편지 작성 완료 페이지 입니다</h1>;
+const LetterCompleteRegistPage = () => {
+  const handleSendMessage = () => {
+    console.log('a');
+  };
+
+  return (
+    <>
+      <h1>편지 작성이 완료되었습니다.</h1>
+      <p>메세지 전송 url</p>
+      <input value="" placeholder="메세지 전송 url" type="text" />
+      <button type="button" onClick={handleSendMessage}>카카오톡으로 메세지 전송하기</button>
+    </>
+  );
+};
 
 export default LetterCompleteRegistPage;
--- front-end-app/src/pages/letter/LetterRegistPage.jsx
+++ front-end-app/src/pages/letter/LetterRegistPage.jsx
@@ -1,5 +1,42 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { UserContext } from 'contexts/UserContext';
+import { useNavigate } from 'react-router-dom';
+import useApi from 'hooks/useApi';
 
-const LetterRegistPage = () => <h1>편지 작성 페이지 입니다</h1>;
+const LetterRegistPage = () => {
+  const { userInfo } = useContext(UserContext);
+  const navigate = useNavigate();
+  const { data, isLoading, error } = useApi({ url: '/board/592', method: 'GET' });
+
+  const handleComplete = () => {
+    navigate('/letter/complete');
+  };
+  // 로그인 안했을 경우 자동으로 로그인 페이지로 이동
+  // useEffect(() => {
+  //   if (!userInfo) {
+  //     navigate('/member/login'); // '/member/login'으로 이동
+  //     // navigate(-1); // 이전 페이지로 이동
+  //   }
+  // }, []);
+
+  //  private String letterNo; 편지 번호
+  //  private String letterReceiveId; 편지 수신 아이디
+  // private String letterSendId; 편지 송신 아이디
+  // private String letterTitle; 편지 제목
+  // private String letterContent; 편지 내용
+  // private String letterUrl; 편지 URL
+  //  private String registDt; 편지 작성일자
+  // private String kakaoSendYn; 카카오톡 송신여부
+  // private String letterColorNo; 편지 배경번호
+  // private String letterReceiveYn; 받은 편지 열람여부
+  // private String accessToken; 엑세스토큰
+
+  return (
+    <>
+      <h1>편지 작성 페이지 입니다</h1>
+      <button type="button" onClick={handleComplete}>편지 작성 완료</button>
+    </>
+  );
+};
 
 export default LetterRegistPage;
