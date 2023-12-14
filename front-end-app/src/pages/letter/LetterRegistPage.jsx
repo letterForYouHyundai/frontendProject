ed5598481391eb732384efc0aecf1c6ab717e41d
codeSweet0828 <codesweet0828@gmail.com>
2023-12-14 10:31:05 +0900

    체크박스 존재시 회원대상발송여부

--- front-end-app/src/pages/letter/LetterRegistPage.jsx
+++ front-end-app/src/pages/letter/LetterRegistPage.jsx
@@ -5,6 +5,7 @@ import useApi from 'hooks/useApi';
 import LetterTemplate from 'components/letter/LetterTemplate';
 import DraggableColorPicker from 'components/letter/DraggableColorPicker';
 import { MyButton } from 'styles/components/commons/ButtonStyles';
+import LetterMemberrConfirm from 'pages/letter/LetterMemberConfirm';
 
 const LetterRegistPage = () => {
   const { userInfo } = useContext(UserContext);
@@ -17,6 +18,8 @@ const LetterRegistPage = () => {
   const [memberYn, setMemberYn] = useState('');
   const [letterUrl, setLetterUrl] = useState('');
   const [letterNo, setLetterNo] = useState('');
+  const [isChecked, setIsChecked] = useState(false);
+  const [inputValue, setInputValue] = useState('');
 
   // color
   const [colorInfo, setColorInfo] = useState({
@@ -50,20 +53,26 @@ const LetterRegistPage = () => {
       alert('보내는 대상의 이름을 입력해주세요.');
       return;
     }
-    // 나중에 validation check박스 추가시 있는 경우에는 아이디 validation 체크
-    /* if (memberYn === null) {
-      alert('보내는 대상이 회원인지 조회해주세요.');
-      return;
-    }
-    if (memberYn === 'N') {
-      alert('보내는 대상이 회원이 아닙니다.');
-      return;
-    } */
 
+    // 나중에 validation check박스 추가시 있는 경우에는 아이디 validation 체크
+    if (isChecked === true) {
+      if (!inputValue.trim()) {
+        alert('회원 이메일을 입력해주세요.');
+        return;
+      }
+      if (memberYn === null) {
+        alert('보내는 대상이 회원인지 조회해주세요.');
+        return;
+      }
+      if (memberYn === 'N') {
+        alert('보내는 대상이 회원이 아닙니다.');
+        return;
+      }
+    }
     const letterData = {
       letterTitle: title,
       letterContent: content,
-      // letterSendId: userName,
+      letterSendId: inputValue,
       userAlias: userName, // receiveUserAlias
       letterColorNo: colorInfo.colorNo,
     };
@@ -87,8 +96,6 @@ const LetterRegistPage = () => {
 
   useEffect(() => {
     if (data != null || data === undefined) {
-      console.log('회원여부 조회 성공');
-      console.log(data);
       if (data.checkMemberYn === 'N') {
         setMemberYn('N');
       } else {
@@ -104,7 +111,6 @@ const LetterRegistPage = () => {
   }, [data]);
   useEffect(() => {
     if (letterUrl !== null && letterUrl !== undefined && letterUrl !== '' && letterNo !== null && letterNo !== undefined && letterNo !== '') {
-      console.log(`Navigating to /letter/complete with letterUrl: ${letterUrl} and letterNo: ${letterNo}`);
       navigate('/letter/complete', { state: { letterUrl, letterNo } });
     }
   }, [letterUrl, letterNo]);
@@ -117,7 +123,7 @@ const LetterRegistPage = () => {
     // console.log(`receiveEmail:  ${receiveEmail}`);
     // 이후에 회원아이디가 변경되면 해당 설정 추가
     setMemberYn('N');
-    setApiCall({ ...apiCall, method: 'GET', url: '/member/checkMemberYn?userEmail=2345' });
+    setApiCall({ ...apiCall, method: 'GET', url: `/member/checkMemberYn?userEmail=${inputValue}` });
   };
 
   //  private String letterNo; 편지 번호
@@ -154,6 +160,12 @@ const LetterRegistPage = () => {
     <>
       <DraggableColorPicker onPickerClick={handlePickerClick} />
       <LetterTemplate from={false} title={title} content={content} userName={userName} colorInfo={colorInfo} onChangeTitle={handleTitleText} onChangeContent={handleContentText} onChangeUser={handleUserText} />
+      <LetterMemberrConfirm
+        isChecked={isChecked}
+        setIsChecked={setIsChecked}
+        inputValue={inputValue}
+        setInputValue={setInputValue}
+      />
       <MyButton type="button" onClick={handleComplete} variant="outlined">편지 작성 완료</MyButton>
       <MyButton type="button" onClick={checkMemberYn} variant="outlined">회원 여부 조회</MyButton>
     </>
